df3dbed853d8f43730f02ca2850aea9c3769b515
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2020-04-17 15:36:37 +0200

    remove examples app

--- projects/docs/src/app/app.module.ts
+++ projects/docs/src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExamplesTableModule } from '../table/table.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ExamplesTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
