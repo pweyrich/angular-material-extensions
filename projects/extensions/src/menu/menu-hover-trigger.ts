dd7c7d024012e65bb1684212b74c138a8850ab76
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2020-05-13 15:40:20 +0200

    wip

--- projects/extensions/src/menu/menu-hover-trigger.ts
+++ projects/extensions/src/menu/menu-hover-trigger.ts
@@ -1,27 +1,15 @@
 import {
   Directive,
-  ElementRef,
   HostListener,
-  Inject,
   Input,
-  OnInit,
-  Optional,
-  Self,
-  ViewContainerRef
+  OnInit
 } from '@angular/core';
 import {
-  MAT_MENU_SCROLL_STRATEGY,
-  MatMenu,
-  MatMenuItem,
   MatMenuPanel,
   MatMenuTrigger
 } from '@angular/material/menu';
 import { Subject } from 'rxjs';
-import { Directionality } from '@angular/cdk/bidi';
-import {
-  Overlay,
-  OverlayRef
-} from '@angular/cdk/overlay';
+import { OverlayRef } from '@angular/cdk/overlay';
 import {
   skip,
   switchMap,
@@ -41,6 +29,7 @@ export class MenuHoverTrigger extends MatMenuTrigger implements OnInit
   }
   set hoverMenu(menu: MatMenuPanel)
   {
+    menu.hasBackdrop = true; // force the menu to have a backdrop
     this.menu = menu;
   }
 
@@ -48,17 +37,6 @@ export class MenuHoverTrigger extends MatMenuTrigger implements OnInit
   private mouseLeave$ = new Subject<MouseEvent>();
   private backdropHover$ = new Subject<MouseEvent>();
   private backdropExited$ = new Subject<MouseEvent>();
-
-  constructor(private overlay: Overlay,
-              private element: ElementRef<HTMLElement>,
-              _viewContainerRef: ViewContainerRef,
-              @Inject(MAT_MENU_SCROLL_STRATEGY) scrollStrategy: any,
-              @Optional() private parentMenu: MatMenu,
-              @Optional() @Self() private menuItemInstance: MatMenuItem,
-              @Optional() _dir: Directionality)
-  {
-    super(overlay, element, _viewContainerRef, scrollStrategy, parentMenu, menuItemInstance, _dir);
-  }
 
   public ngOnInit(): void
   {
@@ -84,5 +62,10 @@ export class MenuHoverTrigger extends MatMenuTrigger implements OnInit
     // to figure out why mouseleave event is emitted when opening the menu
     this.mouseEnter$.next(event);
     console.log('entered');
+  }
+
+  @HostListener('mouseleave') _handleMouseLeave(event: MouseEvent) {
+    this.mouseLeave$.next(event);
+    console.log('leave');
   }
 }
