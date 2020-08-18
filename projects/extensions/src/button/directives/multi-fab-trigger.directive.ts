b04427ec52b1eec79f948696938dfd276f378c13
Pascal Weyrich <pascalweyrich@gmx.de>
2020-08-18 11:43:03 +0200

    encapsulate setting the zIndex in the directive

--- projects/extensions/src/button/directives/multi-fab-trigger.directive.ts
+++ projects/extensions/src/button/directives/multi-fab-trigger.directive.ts
@@ -1,36 +1,35 @@
-import {Directive, Input, TemplateRef, HostListener, ViewContainerRef, ElementRef, OnInit, OnDestroy} from '@angular/core';
+import { Directive, Input, TemplateRef, HostListener, ViewContainerRef, ElementRef, OnInit, OnDestroy, HostBinding } from '@angular/core';
 import { TemplatePortal } from '@angular/cdk/portal';
 import { Overlay, OverlayRef } from '@angular/cdk/overlay';
 
 @Directive({
-    selector: 'button[multiFabTriggerFor]',
-    exportAs: 'multiFab'
+  selector: 'button[multiFabTriggerFor]',
+  exportAs: 'multiFab'
 })
-export class MultiFabTriggerDirective implements OnInit, OnDestroy
-{
- @Input('multiFabTriggerFor') template:TemplateRef<any>;
+export class MultiFabTriggerDirective implements OnInit, OnDestroy {
+  @Input('multiFabTriggerFor') template: TemplateRef<any>;
 
- private _portal:TemplatePortal;
- private _overlayRef:OverlayRef;
+  private _portal: TemplatePortal;
+  private _overlayRef: OverlayRef;
 
- constructor(
-     private _elRef:ElementRef,
-     private _vcRef:ViewContainerRef,
-     private _overlay:Overlay
-){}
+  constructor(
+    private _elRef: ElementRef,
+    private _vcRef: ViewContainerRef,
+    private _overlay: Overlay
+  ) { }
 
- ngOnInit() {
+  ngOnInit() {
     this._portal = new TemplatePortal(this.template, this._vcRef);
     this._overlayRef = this._overlay.create({
       positionStrategy: this._overlay.position().flexibleConnectedTo(this._elRef).withPositions([{
-          originX: 'center',
-          originY: 'top',
-          overlayX: 'center',
-          overlayY: 'bottom'
+        originX: 'center',
+        originY: 'top',
+        overlayX: 'center',
+        overlayY: 'bottom'
       }]),
-      hasBackdrop: true,
-      panelClass: 'multi-fab'
+      hasBackdrop: true
     });
+    this._overlayRef.overlayElement.style.display = 'grid';
     this._overlayRef.backdropClick().subscribe(() => this.close());
   }
 
@@ -38,19 +37,29 @@ export class MultiFabTriggerDirective implements OnInit, OnDestroy
     this._overlayRef.dispose();
   }
 
- @HostListener('click')
- public open():void
- {
-    this._overlayRef.attach(this._portal);
- }
+  @HostListener('click')
+  public onHostClick(): void {
+    if (!this.isOpen) {
+      this.open();
+    } else {
+      this.close();
+    }
+  }
 
- public close():void
- {
+  public open(): void {
+      this._overlayRef.attach(this._portal);
+  }
+
+  public close(): void {
     this._overlayRef.detach()
- }
+  }
 
- get isOpen():boolean
- {
-     return this._overlayRef?.hasAttached();
- }
+  get isOpen(): boolean {
+    return this._overlayRef?.hasAttached();
+  }
+
+  @HostBinding('style.zIndex')
+  get zIndex(): number {
+    return this.isOpen ? 1001 : null;
+  }
 }
