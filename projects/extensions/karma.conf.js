7fec631eeb9aa5485742c1b5625d13c84d9e296c
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2020-12-01 15:54:48 +0100

    use karma-coverage instead of karma-coverage-instanbul-reporter

--- projects/extensions/karma.conf.js
+++ projects/extensions/karma.conf.js
@@ -9,18 +9,20 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/extensions'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    reporters: ['progress', 'coverage'],
+    preprocessors: {
+      'src/**/*.js': ['coverage']
     },
-    reporters: ['progress', 'kjhtml'],
+    coverageReporter: {
+      type : 'html',
+      dir : '../../coverage/extensions'
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
