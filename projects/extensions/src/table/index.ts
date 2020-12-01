0c680ff075e121d44cca301883a7898736b7d989
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2020-12-01 14:09:48 +0100

    remove CustomHeaderDirective since this is possible with cdk's drag&drop now.

--- projects/extensions/src/table/index.ts
+++ projects/extensions/src/table/index.ts
@@ -3,6 +3,3 @@ export { MatTableExtensionsModule } from './table.module';
 
 // components
 export { MatSelectionColumnComponent } from './selection-column/selection-column.component';
-
-// directives
-export { CustomHeaderDirective } from './directives/custom-header.directive';
--- projects/extensions/src/table/table.module.ts
+++ projects/extensions/src/table/table.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 
-import { CustomHeaderDirective } from './directives/custom-header.directive';
 import { MatSelectionColumnComponent } from './selection-column/selection-column.component';
 
 
 /** @description Exports all the extensions for material's table component. */
 @NgModule({
   declarations: [
-    CustomHeaderDirective,
     MatSelectionColumnComponent
   ],
   imports: [
@@ -19,7 +17,6 @@ import { MatSelectionColumnComponent } from './selection-column/selection-column
     MatCheckboxModule
   ],
   exports: [
-    CustomHeaderDirective,
     MatSelectionColumnComponent
   ]
 })
